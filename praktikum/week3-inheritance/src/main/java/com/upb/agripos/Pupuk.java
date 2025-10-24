f294d8be80014acc09a42bccd46b13d6b4b7d241
novifitriyani <novifitriyani1811@gmail.com>
2025-10-24 19:39:06 +0700

    Create Pupuk.java

---
+++ praktikum/week3-inheritance/src/main/java/com/upb/agripos/Pupuk.java
@@ -0,0 +1,13 @@
+package main.java.com.upb.agripos.model;
+
+public class Pupuk extends Produk {
+    private String jenis;
+
+    public Pupuk(String kode, String nama, double harga, int stok, String jenis) {
+        super(kode, nama, harga, stok);
+        this.jenis = jenis;
+    }
+
+    public String getJenis() { return jenis; }
+    public void setJenis(String jenis) { this.jenis = jenis; }
+}
