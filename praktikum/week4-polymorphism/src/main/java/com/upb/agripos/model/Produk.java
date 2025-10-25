0117d21f5551462cd3a9f801e4b6cb89777556cd
novifitriyani <novifitriyani1811@gmail.com>
2025-10-25 10:39:50 +0700

    Create Produk.java

---
+++ praktikum/week4-polymorphism/src/main/java/com/upb/agripos/model/Produk.java
@@ -0,0 +1,36 @@
+package main.java.com.upb.agripos.model;
+
+public class Produk {
+    private String kode;
+    private String nama;
+    private double harga;
+    private int stok;
+
+    public Produk(String kode, String nama, double harga, int stok) {
+        this.kode = kode;
+        this.nama = nama;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public double getStok() {
+        return stok;
+    }
+
+
+    public void tambahStok(int jumlah) {
+        this.stok += jumlah;
+    }
+
+    public void tambahStok(double jumlah) {
+        this.stok += (int) jumlah;
+    }
+
+    public String getInfo() {
+        return "Produk: " + nama + " (Kode: " + kode + "), Harga: Rp" + harga + ", Stok: " + stok;
+    }
+}
