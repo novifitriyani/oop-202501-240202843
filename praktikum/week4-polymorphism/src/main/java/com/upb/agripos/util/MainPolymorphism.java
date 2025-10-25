1c41a18a774b111b634cf3fc1afffe66dd76b92c
novifitriyani <novifitriyani1811@gmail.com>
2025-10-25 10:43:57 +0700

    Create MainPolymorphism.java

---
+++ praktikum/week4-polymorphism/src/main/java/com/upb/agripos/util/MainPolymorphism.java
@@ -0,0 +1,20 @@
+package main.java.com.upb.agripos;
+
+import main.java.com.upb.agripos.model.*;
+import main.java.com.upb.agripos.util.CreditBy;
+
+public class MainPolymorphism {
+    public static void main(String[] args) {
+        Produk[] daftarProduk = {
+            new Benih("BNH-001", "Benih Padi IR64", 25000, 100, "IR64"),
+            new Pupuk("PPK-101", "Pupuk Urea", 350000, 40, "Urea"),
+            new AlatPertanian("ALT-501", "Cangkul Baja", 90000, 15, "Baja")
+        };
+
+        for (Produk p : daftarProduk) {
+            System.out.println(p.getInfo()); // Dynamic Binding
+        }
+
+        CreditBy.print("240202843", "Novi Fitriyani");
+    }
+}
