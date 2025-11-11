7a6e0911bfe3be2614645570e8504d8289a78403
novifitriyani <novifitriyani1811@gmail.com>
2025-11-12 00:10:59 +0700

    week5-abstraction-interface

--- praktikum/week5-abstraction-interface/src/main/java/com/upb/agripos/model/pembayaran/Pembayaran.java
+++ praktikum/week5-abstraction-interface/src/main/java/com/upb/agripos/model/pembayaran/Pembayaran.java
@@ -3,7 +3,7 @@ package com.upb.agripos.model.pembayaran;
 public abstract class Pembayaran {
     protected String invoiceNo;
     protected double total;
-
+ 
     public Pembayaran(String invoiceNo, double total) {
         this.invoiceNo = invoiceNo;
         this.total = total;
